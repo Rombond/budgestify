f4f9ef60acd26b5671462bfeae47e1c57b00cf19
Rombond <rombond@live.fr>
2025-01-20 22:16:10 -0500

    Optimize InitDatabase

--- internal/sql/sqlInit.go
+++ internal/sql/sqlInit.go
@@ -9,6 +9,22 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
+type Pair struct {
+	Key   string
+	Value string
+}
+
+var Tables = []Pair{
+	{"User", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, hash BINARY(64) NOT NULL"},
+	{"AccountHouse", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, amount int NOT NULL"},
+	{"House", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, account INT, FOREIGN KEY (account) REFERENCES AccountHouse(id)"},
+	{"Category", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, icons VARCHAR(255), parent INT, house INT, FOREIGN KEY (parent) REFERENCES Category(id), FOREIGN KEY (house) REFERENCES House(id)"},
+	{"House_User", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), house INT NOT NULL, user INT NOT NULL, FOREIGN KEY (house) REFERENCES House(id), FOREIGN KEY (user) REFERENCES User(id)"},
+	{"Account", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, amount int NOT NULL, house_user INT NOT NULL, FOREIGN KEY (house_user) REFERENCES House_User(id)"},
+	{"Transaction", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, amount int NOT NULL, payer INT NOT NULL, category INT, FOREIGN KEY (payer) REFERENCES House_User(id), FOREIGN KEY (category) REFERENCES Category(id)"},
+	{"Participant", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), house_user INT NOT NULL, transaction int NOT NULL,FOREIGN KEY (house_user) REFERENCES House_User(id), FOREIGN KEY (transaction) REFERENCES Transaction(id)"},
+}
+
 func ConnectDatabase() *sql.DB {
 	cfg := mysql.Config{
 		User:   "root",
@@ -20,68 +36,34 @@ func ConnectDatabase() *sql.DB {
 
 	db, err := sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
-		slog.Error(err.Error())
+		slog.Error("[ConnectDatabase] Error on Open: " + err.Error())
 		os.Exit(1)
 	}
 	return db
 }
 
 func InitDatabase(db *sql.DB) {
-	tableName := "User"
-	if isTableMissing(db, tableName) {
-		createTable(db, tableName, "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, hash BINARY(64) NOT NULL")
-	}
-
-	tableName = "AccountHouse"
-	if isTableMissing(db, tableName) {
-		createTable(db, tableName, "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, amount int NOT NULL")
-	}
-
-	tableName = "House"
-	if isTableMissing(db, tableName) {
-		createTable(db, tableName, "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, account INT, FOREIGN KEY (account) REFERENCES AccountHouse(id)")
-	}
-
-	tableName = "Category"
-	if isTableMissing(db, tableName) {
-		createTable(db, tableName, "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, icons VARCHAR(255), parent INT, house INT, FOREIGN KEY (parent) REFERENCES Category(id), FOREIGN KEY (house) REFERENCES House(id)")
-	}
-
-	tableName = "House_User"
-	if isTableMissing(db, tableName) {
-		createTable(db, tableName, "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), house INT NOT NULL, user INT NOT NULL, FOREIGN KEY (house) REFERENCES House(id), FOREIGN KEY (user) REFERENCES User(id)")
-	}
-
-	tableName = "Account"
-	if isTableMissing(db, tableName) {
-		createTable(db, tableName, "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, amount int NOT NULL, house_user INT NOT NULL, FOREIGN KEY (house_user) REFERENCES House_User(id)")
-	}
-
-	tableName = "Transaction"
-	if isTableMissing(db, tableName) {
-		createTable(db, tableName, "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, amount int NOT NULL, payer INT NOT NULL, category INT, FOREIGN KEY (payer) REFERENCES House_User(id), FOREIGN KEY (category) REFERENCES Category(id)")
-	}
-
-	tableName = "Participant"
-	if isTableMissing(db, tableName) {
-		createTable(db, tableName, "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), house_user INT NOT NULL, transaction int NOT NULL,FOREIGN KEY (house_user) REFERENCES House_User(id), FOREIGN KEY (transaction) REFERENCES Transaction(id)")
+	for _, pair := range Tables {
+		if IsTableMissing(db, pair.Key) {
+			createTable(db, pair.Key, pair.Value)
+		}
 	}
 }
 
-func isTableMissing(db *sql.DB, wantedTable string) bool {
+func IsTableMissing(db *sql.DB, wantedTable string) bool {
 	var tableName string
 	isMissing := true
 	query := fmt.Sprintf("show tables like '%s'", wantedTable)
 	results, err := db.Query(query)
 	if err != nil {
-		slog.Error("[isTableMissing] Error querying tables: " + err.Error())
+		slog.Error("[IsTableMissing] Error querying tables: " + err.Error())
 		os.Exit(1)
 	}
 	defer results.Close()
 	for results.Next() {
 		err = results.Scan(&tableName)
 		if err != nil {
-			slog.Error("[isTableMissing] Error querying tables: " + err.Error())
+			slog.Error("[IsTableMissing] Error querying tables: " + err.Error())
 			os.Exit(1)
 		}
 		if tableName == wantedTable {
@@ -90,7 +72,7 @@ func isTableMissing(db *sql.DB, wantedTable string) bool {
 		}
 	}
 	if err = results.Err(); err != nil {
-		slog.Error("[isTableMissing] Error on results: " + err.Error())
+		slog.Error("[IsTableMissing] Error on results: " + err.Error())
 		os.Exit(1)
 	}
 	return isMissing
