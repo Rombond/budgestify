e6d318dd78ee4474bf1d41ddc23bfa497363df45
Rombond <rombond@live.fr>
2025-02-03 19:59:38 -0500

    accountHouse full

---
+++ internal/handlers/accountHouse/accountHouse.go
@@ -0,0 +1,157 @@
+package accountHouseHandlers
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/Rombond/budgestify/api/types/accountHouse"
+	db_sql "github.com/Rombond/budgestify/internal/sql"
+	"github.com/Rombond/budgestify/internal/token"
+	"github.com/gin-gonic/gin"
+)
+
+func GetAccountHouse(db *sql.DB) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		var params accountHouse.AccountHouse
+		err := ctx.ShouldBindJSON(&params)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
+		}
+
+		if params.UserId == 0 {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": "No userID provided"})
+			return
+		}
+
+		valid, err := token.IsTokenValid(ctx.GetHeader("Authorization"), params.UserId)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
+		}
+		if !valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"reason": "Authorization is not valid"})
+			return
+		}
+
+		house, err := db_sql.GetAccountHouse(db, params.Id)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
+		}
+		house.UserId = params.UserId
+		ctx.JSON(http.StatusOK, house)
+	}
+}
+
+func CreateAccountForHouse(db *sql.DB) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		var params accountHouse.AccountHouseForm
+		err := ctx.ShouldBindJSON(&params)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
+		}
+
+		if params.UserId == 0 {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": "No userID provided"})
+			return
+		}
+
+		valid, err := token.IsTokenValid(ctx.GetHeader("Authorization"), params.UserId)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
+		}
+		if !valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"reason": "Authorization is not valid"})
+			return
+		}
+
+		if params.HouseID == 0 {
+			//Try to get house from user
+			houseIDs, err := db_sql.GetHouseIDFromUser(db, params.UserId)
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+				return
+			}
+			if len(houseIDs) == 1 {
+				params.HouseID = houseIDs[0]
+			} else {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"reason": "Can't create an account, multiple houseIDs found"})
+				return
+			}
+			if params.HouseID == 0 {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"reason": "Can't create an account, no houseID found"})
+				return
+			}
+		} else if params.Name == "" {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": "Need 'name' to create an account"})
+			return
+		}
+
+		id, err := db_sql.CreateAccountHouse(db, params.Name, params.Amount)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusCreated, gin.H{"accountHouseID": id})
+	}
+}
+
+func UpdateAccountForHouse(db *sql.DB) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		var params accountHouse.AccountHouse
+		err := ctx.ShouldBindJSON(&params)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
+		}
+
+		if params.UserId == 0 {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": "No userID provided"})
+			return
+		}
+
+		if params.Id == 0 {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": "No houseAccountID found"})
+			return
+		}
+
+		valid, err := token.IsTokenValid(ctx.GetHeader("Authorization"), params.UserId)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
+		}
+		if !valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"reason": "Authorization is not valid"})
+			return
+		}
+
+		if params.Amount != 0 {
+			_, err = db_sql.ChangeAccountHouseAmount(db, params.Id, params.Amount)
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+				return
+			}
+		}
+
+		if params.Name != "" {
+			_, err = db_sql.ChangeAccountHouseName(db, params.Id, params.Name)
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+				return
+			}
+		}
+
+		accountHouse, err := db_sql.GetAccountHouse(db, params.Id)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, accountHouse)
+	}
+}
+
+//TODO: Delete account
