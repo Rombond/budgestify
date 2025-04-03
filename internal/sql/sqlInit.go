39e9b3d8d79ae3bbf9031c707fbc028353012b3a
Rombond <rombond@live.fr>
2025-04-03 16:34:35 -0400

    tables reworks

--- internal/sql/sqlInit.go
+++ internal/sql/sqlInit.go
@@ -15,13 +15,90 @@ type Pair struct {
 }
 
 var Tables = []Pair{
-	{"User", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, login VARCHAR(255) NOT NULL, hash BINARY(64) NOT NULL"},
-	{"House", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, account INT, FOREIGN KEY (account) REFERENCES AccountHouse(id)"},
-	{"Category", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, icons VARCHAR(255), parent INT, house INT, FOREIGN KEY (parent) REFERENCES Category(id), FOREIGN KEY (house) REFERENCES House(id)"},
-	{"House_User", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), house INT NOT NULL, user INT NOT NULL, admin BOOLEAN NOT NULL, FOREIGN KEY (house) REFERENCES House(id), FOREIGN KEY (user) REFERENCES User(id)"},
-	{"Account", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, amount FLOAT NOT NULL, currency CHAR(3) NOT NULL, theoricalAmount FLOAT NOT NULL, house_user INT NOT NULL, FOREIGN KEY (house_user) REFERENCES House_User(id)"},
-	{"Transaction", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(255) NOT NULL, amount FLOAT NOT NULL, payer INT NOT NULL, payer_account INT, house_account INT, category INT, pay_date DATE NOT NULL, currency CHAR(3) NOT NULL, conversion_rate FLOAT NOT NULL DEFAULT 1, FOREIGN KEY (payer) REFERENCES House_User(id), FOREIGN KEY (payer_account) REFERENCES Account(id), FOREIGN KEY (house_account) REFERENCES AccountHouse(id), FOREIGN KEY (category) REFERENCES Category(id)"},
-	{"Participant", "id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), house_user INT NOT NULL, transaction INT NOT NULL, has_repay BOOLEAN NOT NULL, repay_date DATE, FOREIGN KEY (house_user) REFERENCES House_User(id), FOREIGN KEY (transaction) REFERENCES Transaction(id)"},
+	{"User", `
+		id INT NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(id), 
+		name VARCHAR(255) NOT NULL, 
+		login VARCHAR(255) NOT NULL, 
+		hash BINARY(64) NOT NULL
+	`},
+	{"House", `
+		id INT NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(id), 
+		name VARCHAR(255) NOT NULL, 
+		account INT
+	`},
+	// FOREIGN KEY (account) REFERENCES Account(id) added later
+	{"Category", `
+		id INT NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(id), 
+		name VARCHAR(255) NOT NULL, 
+		icons VARCHAR(255), 
+		parent INT, 
+		house INT NOT NULL, 
+		FOREIGN KEY (parent) REFERENCES Category(id), 
+		FOREIGN KEY (house) REFERENCES House(id)
+	`},
+	{"House_User", `
+		id INT NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(id), 
+		house INT NOT NULL, 
+		user INT NOT NULL, 
+		admin BOOLEAN NOT NULL DEFAULT false, 
+		FOREIGN KEY (house) REFERENCES House(id), 
+		FOREIGN KEY (user) REFERENCES User(id)
+	`},
+	{"Account", `
+		id INT NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(id), 
+		name VARCHAR(255) NOT NULL, 
+		house_user INT NOT NULL, 
+		amount FLOAT NOT NULL, 
+		currency CHAR(3) NOT NULL, 
+		theoricalAmount FLOAT, 
+		FOREIGN KEY (house_user) REFERENCES House_User(id)
+	`},
+	{"Transaction", `
+		id INT NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(id), 
+		name VARCHAR(255) NOT NULL, 
+		category INT, 
+		amount FLOAT NOT NULL, 
+		payer INT NOT NULL, 
+		payer_account INT, 
+		pay_date DATE NOT NULL, 
+		currency CHAR(3) NOT NULL, 
+		conversion_rate FLOAT NOT NULL DEFAULT 1, 
+		FOREIGN KEY (payer) REFERENCES House_User(id), 
+		FOREIGN KEY (payer_account) REFERENCES Account(id), 
+		FOREIGN KEY (category) REFERENCES Category(id)
+	`},
+	{"Participant", `
+		id INT NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(id), 
+		transaction INT NOT NULL, 
+		house_user INT NOT NULL, 
+		repay_date DATE, 
+		amount_left FLOAT NOT NULL, 
+		FOREIGN KEY (house_user) REFERENCES House_User(id), 
+		FOREIGN KEY (transaction) REFERENCES Transaction(id)
+	`},
+	{"Recurrence", `
+		id INT NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(id), 
+		name VARCHAR(255) NOT NULL, 
+		house_user INT NOT NULL, 
+		payer_account INT NOT NULL, 
+		category INT, 
+		amount FLOAT NOT NULL, 
+		currency CHAR(3) NOT NULL, 
+		conversion_rate FLOAT NOT NULL DEFAULT 1, 
+		pay_date DATE NOT NULL, 
+		day_cycle INT NOT NULL, 
+		FOREIGN KEY (house_user) REFERENCES House_User(id),
+		FOREIGN KEY (payer_account) REFERENCES Account(id),
+		FOREIGN KEY (category) REFERENCES Category(id)
+	`},
 }
 
 const (
@@ -32,6 +109,7 @@ const (
 	AccountIdx     = 4
 	TransactionIdx = 5
 	ParticipantIdx = 6
+	RecurrenceIdx  = 7
 )
 
 func ConnectDatabase() *sql.DB {
@@ -55,6 +133,14 @@ func InitDatabase(db *sql.DB) {
 	for _, pair := range Tables {
 		if IsTableMissing(db, pair.Key) {
 			createTable(db, pair.Key, pair.Value)
+			if pair.Key == Tables[AccountIdx].Key {
+				query := fmt.Sprintf("ALTER TABLE %s ADD CONSTRAINT account FOREIGN KEY (account) REFERENCES Account(id)", Tables[HouseIdx].Key)
+				_, err := db.Exec(query)
+				if err != nil {
+					slog.Error("[InitDatabase] Error altering" + Tables[HouseIdx].Key + " table, to add foreign key of Account: " + err.Error())
+					os.Exit(1)
+				}
+			}
 		}
 	}
 }
@@ -62,7 +148,7 @@ func InitDatabase(db *sql.DB) {
 func IsTableMissing(db *sql.DB, wantedTable string) bool {
 	var tableName string
 	isMissing := true
-	query := fmt.Sprintf("show tables like '%s'", wantedTable)
+	query := fmt.Sprintf("SHOW TABLES LIKE '%s'", wantedTable)
 	results, err := db.Query(query)
 	if err != nil {
 		slog.Error("[IsTableMissing] Error querying tables: " + err.Error())
