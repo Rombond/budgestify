1978c578ce2233b4ff2643d20ce1b48c6074940c
Rombond <rombond@live.fr>
2025-06-08 13:40:24 -0400

    change type of TheoricalAmount

--- api/types/account/account.go
+++ api/types/account/account.go
@@ -1,12 +1,14 @@
 package account
 
+import "database/sql"
+
 type Account struct {
-	Id              int     `json:"id"`
-	Name            string  `json:"name"`
-	House_User      int     `json:"house_userID"`
-	Amount          float64 `json:"amount"`
-	Currency        string  `json:"currency"`
-	TheoricalAmount float64 `json:"theoricalAmount"`
+	Id              int             `json:"id"`
+	Name            string          `json:"name"`
+	House_User      int             `json:"house_userID"`
+	Amount          float64         `json:"amount"`
+	Currency        string          `json:"currency"`
+	TheoricalAmount sql.NullFloat64 `json:"theoricalAmount"`
 }
 
 type AccountForm struct {
